fd420f9656ef579b724c9ac5a36edf33a96d36e5
roby251 <106534644+roby251@users.noreply.github.com>
2023-12-21 09:04:40 -0300

    PRISMA ADD

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const db = require('../db');
+const prisma = require('@prisma/client');
 
+const prismaDB = new prisma.PrismaClient();
 const router = express.Router();
 
 // Ruta para registrar nuevos usuarios
@@ -10,27 +12,31 @@ router.post('/register', async (req, res) => {
     // Obtener username y password del cuerpo de la solicitud
     const { name, lastname, email, username, password } = req.body;
 
+    if (!name | !lastname | !email | !username | !password) {
+      res.status(400).send('Los campos son obligatorios');
+    }
     // Hashear la contraseña antes de guardarla en la base de datos
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Preparar la consulta SQL para insertar el nuevo usuario
-    const sql = 'INSERT INTO users (name, lastname, email, username, password) VALUES (?,?,?,?,?)';
-
-    // Ejecutar la consulta SQL
-    db.run(sql, [name, lastname, email, username, hashedPassword], function (err) {
-      if (err) {
-        // Manejo de errores al intentar insertar en la base de datos
-        console.error(err);
-        res.status(500).send(err);
-      } else {
-        // Usuario registrado con éxito, devolver el ID del nuevo usuario
-        res.status(201).json({ id: this.lastID });
-      }
+    const user = await prismaDB.user.create({
+      data: {
+        name: name,
+        lastname: lastname,
+        email: email,
+        username: username,
+        password: password,
+      },
     });
+
+    if (!user) {
+      res.status(500).send('No podemos crear el usuario en este momento');
+    }
+
+    res.status(200).send(user); //IMPORTANTE QUITAR LLAVE
   } catch (err) {
     // Manejo de errores generales
     console.error(err);
-    res.status(500).send('Server error');
+    res.status(500).send(err);
   }
 });
 
